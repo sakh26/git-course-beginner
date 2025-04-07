77e8413f423946f81dd8fe5cbff628171807c5be
Magnús Dæhlen <sungamd@gmail.com>
2025-04-07 09:28:39 +0200

    Add more lessongs

---
+++ src/app/lessons.ts
@@ -0,0 +1,45 @@
+// Dummy data for the cards
+export  const cardData = [
+  {
+    title: "What is Git?",
+    description:
+      "Learn about the basics of Git and why it's essential for version control.",
+    link: "/lesson-1",
+  },
+  {
+    title: "Git Installation",
+    description:
+      "Step-by-step guide on how to install Git on your operating system.",
+    link: "/lesson-2",
+  },
+  {
+    title: "Basic Git Commands",
+    description:
+      "Explore common Git commands like commit, push, pull, and branch.",
+    link: "/lesson-3",
+  },
+  {
+    title: "Git Best Practices",
+    description:
+      "Discover useful tips and guidelines for working with Git effectively.",
+    link: "/lesson-4",
+  },
+    {
+    title: "What is Love? ",
+    description:
+      "Learn about love and the wonders of that",
+    link: "/lesson-5",
+  },
+  {
+    title: "Emperor of mankind, dictator or god? ",
+    description:
+      "Like Henry Cavill, lets learn",
+    link: "/lesson-6",
+  },
+  {
+    title: "Votann TM: How they took over the galaxy",
+    description:
+      "Captialism is the best",
+    link: "/lesson-7",
+  }
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,34 +8,8 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { cardData } from "./lessons"
 
-// Dummy data for the cards
-const cardData = [
-  {
-    title: "What is Git?",
-    description:
-      "Learn about the basics of Git and why it's essential for version control.",
-    link: "/lesson-1",
-  },
-  {
-    title: "Git Installation",
-    description:
-      "Step-by-step guide on how to install Git on your operating system.",
-    link: "/lesson-2",
-  },
-  {
-    title: "Basic Git Commands",
-    description:
-      "Explore common Git commands like commit, push, pull, and branch.",
-    link: "/lesson-3",
-  },
-  {
-    title: "Git Best Practices",
-    description:
-      "Discover useful tips and guidelines for working with Git effectively.",
-    link: "/lesson-4",
-  },
-];
 
 export default function Home() {
   return (
